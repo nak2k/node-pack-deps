9bd225fd56664aa2e6a64be9c011bcfa0c6c6579
Kengo Nakatsuka <kengo.nakatsuka@gmail.com>
2018-08-17 08:19:22 +0900

    Case without local packages

--- test/test-packDeps.js
+++ test/test-packDeps.js
@@ -2,6 +2,33 @@ const test = require('tape');
 const { packDeps } = require('..');
 
 test('test packDeps', t => {
+  t.plan(5);
+
+  const options = {
+    pkgJson: {
+      dependencies: {
+        mkdirp: '> 0.4',
+      },
+    },
+    pkgDir: __dirname,
+    production: true,
+    cacheBaseDir: __dirname + '/tmp',
+  };
+
+  packDeps(options, (err, result) => {
+    t.error(err);
+
+    t.equal(typeof(result.cacheDir), 'string');
+    t.equal(typeof(result.cacheFile), 'string');
+
+    const { zip } = result;
+
+    t.equal(typeof(zip), 'object');
+    t.equal(zip.file('node_modules/mkdirp/readme.markdown').name, 'node_modules/mkdirp/readme.markdown');
+  });
+});
+
+test('test packDeps', t => {
   t.plan(6);
 
   const options = {
