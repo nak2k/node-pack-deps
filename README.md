46031952111b73aff6472e9272cfaaa7410031b9
Kengo Nakatsuka <kengo.nakatsuka@gmail.com>
2018-12-03 18:54:38 +0900

    Add options.baseDirInZip

--- README.md
+++ README.md
@@ -22,6 +22,8 @@ Generate a zip file that contains dependencies from a specified `package.json`.
   - A boolean value whether to pack dependencies in production mode.
 - `options.cacheBaseDir`
   - A path of a base directory that cache a packed package on.
+- `options.baseDirInZip`
+  - A path of a base directory in a zip file.
 - `callback(err, { zip, cacheFile, cacheDir })`
   - A function that is callback when packing is completed.
   - `err` - An Error object when an error is occured.
--- test/test-packDeps.js
+++ test/test-packDeps.js
@@ -12,7 +12,7 @@ test('test packDeps', t => {
     },
     pkgDir: __dirname,
     production: true,
-    cacheBaseDir: __dirname + '/tmp',
+    cacheBaseDir: __dirname + '/tmp/packDeps',
   };
 
   packDeps(options, (err, result) => {
@@ -25,5 +25,33 @@ test('test packDeps', t => {
 
     t.equal(typeof(zip), 'object');
     t.equal(zip.file('node_modules/mkdirp/readme.markdown').name, 'node_modules/mkdirp/readme.markdown');
+  });
+});
+
+test('test packDeps with baseDir', t => {
+  t.plan(5);
+
+  const options = {
+    pkgJson: {
+      dependencies: {
+        mkdirp: '> 0.4',
+      },
+    },
+    pkgDir: __dirname,
+    production: true,
+    cacheBaseDir: __dirname + '/tmp/packDeps-with-basedir',
+    baseDirInZip: 'test',
+  };
+
+  packDeps(options, (err, result) => {
+    t.error(err);
+
+    t.equal(typeof(result.cacheDir), 'string');
+    t.equal(typeof(result.cacheFile), 'string');
+
+    const { zip } = result;
+
+    t.equal(typeof(zip), 'object');
+    t.equal(zip.file('test/node_modules/mkdirp/readme.markdown').name, 'test/node_modules/mkdirp/readme.markdown');
   });
 });
