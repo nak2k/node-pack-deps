a9cf846aad2fbcc363e4049911121210ed2a6da4
Kengo Nakatsuka <kengo.nakatsuka@gmail.com>
2018-12-06 22:54:40 +0900

    Add options.noCache

--- README.md
+++ README.md
@@ -24,6 +24,8 @@ Generate a zip file that contains dependencies from a specified `package.json`.
   - A path of a base directory that cache a packed package on.
 - `options.baseDirInZip`
   - A path of a base directory in a zip file.
+- `options.noCache`
+  - If this option is `false`, a zip file is not generated.
 - `callback(err, { zip, cacheFile, cacheDir })`
   - A function that is callback when packing is completed.
   - `err` - An Error object when an error is occured.
--- test/test-packDeps.js
+++ test/test-packDeps.js
@@ -55,3 +55,31 @@ test('test packDeps with baseDir', t => {
     t.equal(zip.file('test/node_modules/mkdirp/readme.markdown').name, 'test/node_modules/mkdirp/readme.markdown');
   });
 });
+
+test('test packDeps without cache', t => {
+  t.plan(5);
+
+  const options = {
+    pkgJson: {
+      dependencies: {
+        mkdirp: '> 0.4',
+      },
+    },
+    pkgDir: __dirname,
+    production: true,
+    cacheBaseDir: __dirname + '/tmp/packDeps-without-cache',
+    noCache: true,
+  };
+
+  packDeps(options, (err, result) => {
+    t.error(err);
+
+    t.equal(typeof(result.cacheDir), 'string');
+    t.equal(typeof(result.cacheFile), 'string');
+
+    const { zip } = result;
+
+    t.equal(typeof(zip), 'object');
+    t.equal(zip.file('node_modules/mkdirp/readme.markdown').name, 'node_modules/mkdirp/readme.markdown');
+  });
+});
